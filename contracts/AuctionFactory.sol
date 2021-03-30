c57c39c69030032448056235938faf254b75b1e6
AndonMitev <mitevandon94@gmail.com>
2021-03-30 18:30:42 +0300

    Remove empty line

--- contracts/AuctionFactory.sol
+++ contracts/AuctionFactory.sol
@@ -515,7 +515,6 @@ contract AuctionFactory is IAuctionFactory, ERC721Holder, Ownable {
         onlyAuctionOwner(_auctionId)
         returns (bool)
     {
-        
         Auction storage auction = auctions[_auctionId];
 
         require(
